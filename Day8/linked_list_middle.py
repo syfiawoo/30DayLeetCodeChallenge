7398e80454bf6da793afd1889b19ee9011c59b9e
fiawoo <syfiawoo@gmail.com>
2020-04-10 11:45:34 -0500

    implemented solution for finding middle of a linked list

---
+++ Day8/linked_list_middle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    @staticmethod
+    def middle_node(head: ListNode) -> ListNode:
+        """
+        :param head:
+        :return:
+        """
+        curr = head  # start from the first item
+        m_list = []  # an empty list to which nodes would be appended
+        # add all elements in linked list to our list
+        while curr:
+            m_list.append(curr)  # add current node to list
+            curr = curr.next  # advance current node to next
+        mid = len(m_list) // 2  # find middle of list
+        return m_list[mid]
