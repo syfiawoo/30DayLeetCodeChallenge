da4f0925955389042572db0072134a28c4649392
fiawoo <syfiawoo@gmail.com>
2020-04-19 12:50:08 -0500

    writing tests for middle node

--- Day8/test_linked_list_middle.py
+++ Day8/test_linked_list_middle.py
@@ -1,4 +1,4 @@
-from pytest import fixture
+from pytest import fixture, mark
 
 from Day8.linked_list_middle import Solution
 
@@ -9,15 +9,23 @@ def create_list():
     return test_list
 
 
-def test_middle_node():
-    assert False
+@fixture
+def add_to_list(create_list, nums):
+    for num in nums:
+        create_list.append_node(num)
+    return create_list
+
+
+@mark.parametrize('nums', [range(1), range(2), range(100), range(105)])
+def test_middle_node(add_to_list, nums):
+    result_list = add_to_list
+    assert Solution.middle_node(result_list.head).val == nums[len(nums) // 2]
 
 
 def test_append_node(create_list):
-    test_list = create_list
-    test_list.append_node(4)
-    test_list.append_node(5)
-    test_list.append_node(6)
-    assert test_list.head.val == 4
-    assert test_list.head.next.val == 5
-    assert test_list.tail.val == 6
+    create_list.append_node(4)
+    create_list.append_node(5)
+    create_list.append_node(6)
+    assert create_list.head.val == 4
+    assert create_list.head.next.val == 5
+    assert create_list.tail.val == 6
