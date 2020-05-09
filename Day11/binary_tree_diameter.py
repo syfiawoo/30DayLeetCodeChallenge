168540d623e4ea6c5b8cd5a4b824e55d8022056c
fiawoo <syfiawoo@gmail.com>
2020-05-09 15:06:13 -0500

    added Day11 starter code

---
+++ Day11/binary_tree_diameter.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameter_of_binary_tree(self, root: TreeNode) -> int:
+        # TODO: implement my solution
+        return 0
